3747993cccd63d60b8668d2d3b8e3919d3037e9f
andrijanasharkoskaaandrijanasharkoskaa <andrijana.sh@gmail.com>
2024-04-06 13:02:55 +0200

    adding footer

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import Nav from "./components/Nav";
 import Main from "./components/Main";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import Reviews from "./components/Reviews";
 function App() {
   return (
     <>
       <Header />
       <Nav />
       <Main />
+      <Reviews />
       <Footer />
     </>
   );
---
+++ src/components/Reviews.js
@@ -0,0 +1,15 @@
+function Reviews() {
+  return (
+    <>
+      <div>
+        <div className="catalog-title">
+          <div className="line"></div>
+          <h3 className="title"> Reviews</h3>
+          <div className="line"></div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Reviews;
