a5b205f297006f1ccc7afe0b3733f26933d49a42
andrijanasharkoskaaandrijanasharkoskaa <andrijana.sh@gmail.com>
2024-04-07 21:29:23 +0200

    adding content to About component

--- src/components/About.js
+++ src/components/About.js
@@ -6,6 +6,36 @@ function About() {
         <h3 className="title"> About Us</h3>
         <div className="line"></div>
       </div>
+
+      <div className="about-us">
+        <div>
+          <img src="https://t3.ftcdn.net/jpg/06/17/13/26/360_F_617132669_YptvM7fIuczaUbYYpMe3VTLimwZwzlWf.jpg" />
+        </div>
+
+        <div className="bio">
+          <h3>Welcome to Our Bookstore</h3>
+          <h5>Here you can shop the best titles at the best prices</h5>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur
+          blandit elit at arcu ornare egestas. Etiam pulvinar magna et mi varius
+          pretium. Aenean imperdiet nunc sed risus consectetur, ac dapibus diam
+          consectetur. Phasellus at enim purus. In hac habitasse platea
+          dictumst. Mauris convallis odio sed vehicula tristique. Integer id
+          risus auctor, aliquet sapien eu, blandit erat. In in ligula ultrices,
+          aliquam felis non, iaculis lectus. Maecenas convallis ipsum quis quam
+          mattis laoreet. Pellentesque nisl neque, pulvinar non varius ut,
+          fringilla a ligula. Donec in viverra tellus, sollicitudin luctus
+          dolor. Etiam at nisi quam. Praesent a lacus id mauris facilisis
+          facilisis nec sit amet ipsum. Sed nec metus neque. Vivamus tempus
+          massa ut urna luctus, et interdum magna porta. Donec vel fringilla
+          metus. Proin condimentum semper massa. Donec suscipit facilisis dui,
+          et maximus turpis laoreet viverra. Vivamus ultricies ipsum nibh, at
+          porttitor felis molestie ac. Integer sollicitudin augue diam, eu
+          tempor purus placerat sed. Suspendisse potenti. Maecenas vel tortor
+          eget diam sodales auctor. Pellentesque egestas lectus semper, pharetra
+          urna nec, facilisis erat.
+          <h5>Contact us at info@bookly.com</h5>
+        </div>
+      </div>
     </>
   );
 }
